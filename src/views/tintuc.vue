62d88a8db6c01921a2c5ba805cfc54df716fc95e
Tran Quang Huy <tranquanghuy160402@gmail.com>
2022-06-16 22:22:50 +0700

    update code tintuc for huy

--- src/views/tintuc.vue
+++ src/views/tintuc.vue
@@ -4,248 +4,255 @@
   <div class="max-w-[1080px] mx-auto">
     <div>
       <div class="justify-center box-content font-bold mt-10">
-        <h2 class="text-left text-lg">TIN NỔI BẬT</h2>
+        <h2 class="text-left text-lg pl-[5%]">TIN NỔI BẬT</h2>
         <div class="bg-[#999] w-full h-px mb-10"></div>
       </div>
       <!-- items -->
       <div class="flex mx-auto text-center justify-center">
-        <div>
-          <img
-            src="/src/assets/img/hinh2.jpg"
-            alt=""
-            class="max-w-full max-h-full h-96"
-          />
-          <h2 class="font-bold">
-            CHÀO THÁNG 3 TƯƠI MỚI VỚI BỘ BA SỮA CHUA TRÁI CÂY TẠI TRUNG NGUYÊN
-            E-COFFEE
-          </h2>
+        <div class="px-[5%] lg:px-0">
+          <img src="/src/assets/img/hinh2.jpg" alt="" class="w-full" />
+          <a href="">
+            <h2 class="font-[600] text-[18px] mt-[5%]">
+              CHÀO THÁNG 3 TƯƠI MỚI VỚI BỘ BA SỮA CHUA TRÁI CÂY TẠI TRUNG NGUYÊN
+              E-COFFEE
+            </h2>
+          </a>
           <div class="bg-black w-10 h-px mx-auto my-3"></div>
-          <p>
+          <a href="" class="hover:text-yellow-400">
             Khởi đầu tháng 3 tươi mới tràn đầy năng lượng, Trung Nguyên E-Coffee
             ra mắt [...]
-          </p>
+          </a>
         </div>
       </div>
 
       <!-- Title -->
       <div class="justify-center box-content font-bold mt-10">
-        <h2 class="text-left text-lg">TIN KHUYẾN MÃI</h2>
-        <h3 class="text-right">Xem tất cả ></h3>
+        <h2 class="pl-[5%] lg:pl-0 text-left text-lg">TIN KHUYẾN MÃI</h2>
+        <h3 class="pr-[5%] lg:pr-0 text-right">Xem tất cả ></h3>
         <div class="bg-black w-full h-px mb-10"></div>
       </div>
 
       <!-- items -->
-      <div class="grid grid-cols-3 gap-x-[2%] text-center">
-        <div class="col-span-1">
-          <img
-            src="src/assets/img/hinh1.jpg"
-            alt=""
-            class="w-full max-h-full h-60"
-          />
-          <h2 class="font-bold">
-            CHÚC MỪNG SINH NHẬT BẠN – TRUNG NGUYÊN E-COFFEE ƯU ĐÃI 30%
-          </h2>
-          <div class="bg-black w-10 h-px mx-auto my-3"></div>
-          <p>
-            Thay cho lời cảm ơn sự tin tưởng, ủng hộ và đồng hành cùng Trung
-            [...]
-          </p>
+      <div class="grid grid-cols-3 px-[5%] lg:px-0 gap-x-[2%] text-center">
+        <div class="col-span-3 shadow-sm lg:col-span-1">
+          <a href="" class="">
+            <img
+              src="src/assets/img/hinh1.jpg"
+              alt=""
+              class="w-full lg:h-[194.39px]"
+            />
+            <h2 class="font-bold mt-[2%]">
+              CHÚC MỪNG SINH NHẬT BẠN – TRUNG NGUYÊN E-COFFEE ƯU ĐÃI 30%
+            </h2>
+            <div class="bg-black w-10 h-px mx-auto my-3"></div>
+            <p class="hover:text-yellow-400">
+              Thay cho lời cảm ơn sự tin tưởng, ủng hộ và đồng hành cùng Trung
+              [...]
+            </p>
+          </a>
         </div>
-        <div class="col-span-1">
-          <img
-            src="src/assets/img/hinh2.jpg"
-            alt=""
-            class="max-w-full max-h-full h-60"
-          />
-          <h2 class="font-bold">
-            CHÀO THÁNG 3 TƯƠI MỚI VỚI BỘ BA SỮA CHUA TRÁI CÂY TẠI TRUNG NGUYÊN
-            E-COFFEE
-          </h2>
-          <div class="bg-black w-10 h-px mx-auto my-3"></div>
-          <p>
-            Khởi đầu tháng 3 tươi mới tràn đầy năng lượng, Trung Nguyên E-Coffee
-            ra mắt [...]
-          </p>
+        <div class="col-span-3 shadow-sm mt-[12%] lg:mt-0 lg:col-span-1">
+          <a href="">
+            <img src="src/assets/img/hinh2.jpg" alt="" class="w-full" />
+            <h2 class="font-bold mt-[2%]">
+              CHÀO THÁNG 3 TƯƠI MỚI VỚI BỘ BA SỮA CHUA TRÁI CÂY TẠI TRUNG NGUYÊN
+              E-COFFEE
+            </h2>
+            <div class="bg-black w-10 h-px mx-auto my-3"></div>
+            <p class="hover:text-yellow-400">
+              Khởi đầu tháng 3 tươi mới tràn đầy năng lượng, Trung Nguyên
+              E-Coffee ra mắt [...]
+            </p>
+          </a>
         </div>
-        <div class="col-span-1">
-          <img
-            src="src/assets/img/hinh3.jpg"
-            alt=""
-            class="max-w-full max-h-full h-60"
-          />
-          <h2 class="font-bold">
-            THÊM BẠN THÊM VUI – NHẬN NGAY ƯU ĐÃI MUA 03 TẶNG 01​
-          </h2>
-          <div class="bg-black w-10 h-px mx-auto my-3"></div>
-          <p>
-            Đồng hành cùng các bạn trẻ trở lại trường học sau những tháng ngày
-            giãn [...]
-          </p>
+        <div class="col-span-3 shadow-sm mt-[12%] lg:mt-0 lg:col-span-1">
+          <a href="">
+            <img src="src/assets/img/hinh3.jpg" alt="" class="w-full" />
+            <h2 class="font-bold mt-[2%]">
+              THÊM BẠN THÊM VUI – NHẬN NGAY ƯU ĐÃI MUA 03 TẶNG 01​
+            </h2>
+            <div class="bg-black w-10 h-px mx-auto my-3"></div>
+            <p class="hover:text-yellow-400">
+              Đồng hành cùng các bạn trẻ trở lại trường học sau những tháng ngày
+              giãn [...]
+            </p>
+          </a>
         </div>
       </div>
 
       <!-- Title -->
       <div class="justify-center box-content font-bold mt-10">
         <h2 class="text-left text-lg">TIN NHƯỢNG QUYỀN</h2>
-        <h3 class="text-right">Xem tất cả ></h3>
+        <a href="">
+          <h3 class="text-right">Xem tất cả ></h3>
+        </a>
+
         <div class="bg-black w-auto h-px mb-10"></div>
       </div>
 
       <!-- items -->
-      <div class="flex flex-row gap-10 text-center">
-        <div>
-          <img
-            src="src/assets/img/hinh4.jpg"
-            alt=""
-            class="max-w-full max-h-full h-60"
-          />
-          <h2 class="font-bold">
-            CHỈ 65 TRIỆU ĐỒNG HỢP TÁC CÙNG THƯƠNG HIỆU TRUNG NGUYÊN
-          </h2>
-          <div class="bg-black w-10 h-px mx-auto my-3"></div>
-          <p>
-            KHÔNG thu phí Nhượng Quyền KHÔNG thu phí quản lý Thời gian HOÀN VỐN
-            NHANH [...]
-          </p>
+      <div class="grid grid-cols-3 px-[5%] lg:px-0 gap-x-[2%] text-center">
+        <div class="col-span-3 shadow-sm mt-[12%] lg:mt-0 lg:col-span-1">
+          <a href="">
+            <img src="src/assets/img/hinh4.jpg" alt="" class="w-full" />
+            <h2 class="font-bold mt-[2%]">
+              CHỈ 65 TRIỆU ĐỒNG HỢP TÁC CÙNG THƯƠNG HIỆU TRUNG NGUYÊN
+            </h2>
+            <div class="bg-black w-10 h-px mx-auto my-3"></div>
+            <p class="hover:text-yellow-400">
+              KHÔNG thu phí Nhượng Quyền KHÔNG thu phí quản lý Thời gian HOÀN
+              VỐN NHANH [...]
+            </p>
+          </a>
         </div>
-        <div>
-          <img
-            src="src/assets/img/hinh5.jpg"
-            alt=""
-            class="max-w-full max-h-full h-60"
-          />
-          <h2 class="font-bold">
-            CÙNG HIỆN THỰC HOÁ KHÁT VỌNG KHỞI NGHIỆP KINH DOANH VỚI NGÀNH CÀ PHÊ
-          </h2>
-          <div class="bg-black w-10 h-px mx-auto my-3"></div>
-          <p>
-            Nhân dịp kỉ niệm 25 năm sáng tạo và phát triển của Tập đoàn Trung
-            [...]
-          </p>
+        <div class="col-span-3 shadow-sm mt-[12%] lg:mt-0 lg:col-span-1">
+          <a href="">
+            <img src="src/assets/img/hinh5.jpg" alt="" class="w-full" />
+            <h2 class="font-bold mt-[2%]">
+              CÙNG HIỆN THỰC HOÁ KHÁT VỌNG KHỞI NGHIỆP KINH DOANH VỚI NGÀNH CÀ
+              PHÊ
+            </h2>
+            <div class="bg-black w-10 h-px mx-auto my-3"></div>
+            <p class="hover:text-yellow-400">
+              Nhân dịp kỉ niệm 25 năm sáng tạo và phát triển của Tập đoàn Trung
+              [...]
+            </p>
+          </a>
         </div>
-        <div>
-          <img
-            src="src/assets/img/hinh6.jpg"
-            alt=""
-            class="max-w-full max-h-full h-60"
-          />
-          <h2 class="font-bold">
-            KHỞI NGHIỆP THÀNH CÔNG – GIÀU CÓ BỀN VỮNG CÙNG TRUNG NGUYÊN E-COFFEE
-          </h2>
-          <div class="bg-black w-10 h-px mx-auto my-3"></div>
-          <p>
-            Với lợi thế thương hiệu dẫn đầu , chuyên gia cà phê hàng đầu với
-            [...]
-          </p>
+        <div class="col-span-3 shadow-sm mt-[12%] lg:mt-0 lg:col-span-1">
+          <a href="">
+            <img
+              src="src/assets/img/hinh6.jpg"
+              alt=""
+              class="w-full lg:h-[194.39px]"
+            />
+            <h2 class="font-bold mt-[2%]">
+              KHỞI NGHIỆP THÀNH CÔNG – GIÀU CÓ BỀN VỮNG CÙNG TRUNG NGUYÊN
+              E-COFFEE
+            </h2>
+            <div class="bg-black w-10 h-px mx-auto my-3"></div>
+            <p class="hover:text-yellow-400">
+              Với lợi thế thương hiệu dẫn đầu , chuyên gia cà phê hàng đầu với
+              [...]
+            </p>
+          </a>
         </div>
       </div>
 
       <!-- Title -->
       <div class="justify-center box-content font-bold mt-10">
         <h2 class="text-left text-lg">THƯ VIỆN KIẾN THỨC</h2>
-        <h3 class="text-right">Xem tất cả ></h3>
+        <a href="">
+          <h3 class="text-right">Xem tất cả ></h3>
+        </a>
         <div class="bg-black w-auto h-px mb-10"></div>
       </div>
 
       <!-- items -->
-      <div class="flex flex-row gap-10 text-center">
-        <div>
-          <img
-            src="src/assets/img/hinh7.jpg"
-            alt=""
-            class="max-w-full max-h-full h-60"
-          />
-          <h2 class="font-bold">Cà Phê Chai Hạt Mộc Trung Nguyên E-Coffee</h2>
-          <div class="bg-black w-10 h-px mx-auto my-3"></div>
-          <p>
-            ản phẩm Cà Phê Chai Hạt Mộc Trung Nguyên E-Coffee được nhiều khách
-            hàng [...]
-          </p>
+      <div class="grid grid-cols-3 px-[5%] lg:px-0 gap-x-[2%] text-center">
+        <div class="col-span-3 shadow-sm mt-[12%] lg:mt-0 lg:col-span-1">
+          <a href="">
+            <img
+              src="src/assets/img/hinh7.jpg"
+              alt=""
+              class="w-full lg:h-[276.48px]"
+            />
+            <h2 class="font-bold mt-[2%]">
+              Cà Phê Chai Hạt Mộc Trung Nguyên E-Coffee
+            </h2>
+            <div class="bg-black w-10 h-px mx-auto my-3"></div>
+            <p class="hover:text-yellow-400">
+              ản phẩm Cà Phê Chai Hạt Mộc Trung Nguyên E-Coffee được nhiều khách
+              hàng [...]
+            </p>
+          </a>
         </div>
-        <div>
-          <img
-            src="src/assets/img/hinh8.jpg"
-            alt=""
-            class="max-w-full max-h-full h-60"
-          />
-          <h2 class="font-bold">
-            Sẻ Chia Năng Lượng Ngọt Ngào Với Pudding Legend Cà Phê Sữa Đá
-          </h2>
-          <div class="bg-black w-10 h-px mx-auto my-3"></div>
-          <p>
-            Sẻ chia năng lượng ngọt ngào với Pudding Legend Cà phê sữa đá Là một
-            [...]
-          </p>
+        <div class="col-span-3 shadow-sm mt-[12%] lg:mt-0 lg:col-span-1">
+          <a href="">
+            <img src="src/assets/img/hinh8.jpg" alt="" class="w-full" />
+            <h2 class="font-bold mt-[2%]">
+              Sẻ Chia Năng Lượng Ngọt Ngào Với Pudding Legend Cà Phê Sữa Đá
+            </h2>
+            <div class="bg-black w-10 h-px mx-auto my-3"></div>
+            <p class="hover:text-yellow-400">
+              Sẻ chia năng lượng ngọt ngào với Pudding Legend Cà phê sữa đá Là
+              một [...]
+            </p>
+          </a>
         </div>
-        <div>
-          <img
-            src="src/assets/img/hinh9.jpg"
-            alt=""
-            class="max-w-full max-h-full h-60"
-          />
-          <h2 class="font-bold">
-            Nạp Năng Lượng Sáng Tạo, Tươi Trẻ Với Cà Phê Trung Nguyên Legend
-            Passiona Khổ Qua
-          </h2>
-          <div class="bg-black w-10 h-px mx-auto my-3"></div>
-          <p>
-            Nạp năng lượng sáng tạo, tươi trẻ với Cà phê Trung Nguyên Legend
-            Passiona khổ [...]
-          </p>
+        <div class="col-span-3 shadow-sm mt-[12%] lg:mt-0 lg:col-span-1">
+          <a href="">
+            <img src="src/assets/img/hinh9.jpg" alt="" class="w-full" />
+            <h2 class="font-bold mt-[2%]">
+              Nạp Năng Lượng Sáng Tạo, Tươi Trẻ Với Cà Phê Trung Nguyên Legend
+              Passiona Khổ Qua
+            </h2>
+            <div class="bg-black w-10 h-px mx-auto my-3"></div>
+            <p class="hover:text-yellow-400">
+              Nạp năng lượng sáng tạo, tươi trẻ với Cà phê Trung Nguyên Legend
+              Passiona khổ [...]
+            </p>
+          </a>
         </div>
       </div>
 
       <!-- Title -->
       <div class="justify-center box-content font-bold mt-10">
         <h2 class="text-left text-lg">VỀ TẬP ĐOÀN TRUNG NGUYÊN LEGEND</h2>
-        <h3 class="text-right">Xem tất cả ></h3>
+        <a href="">
+          <h3 class="text-right">Xem tất cả ></h3>
+        </a>
         <div class="bg-black w-auto h-px mb-10"></div>
       </div>
 
       <!-- items -->
-      <div class="flex flex-row gap-10 text-center mb-8">
-        <div>
-          <img
-            src="src/assets/img/hinh10.png"
-            alt=""
-            class="max-w-full max-h-full h-60"
-          />
-          <h2 class="font-bold">
-            MV Hướng Dương – Wowy x Trung Nguyên Legend!
-          </h2>
-          <div class="bg-black w-10 h-px mx-auto my-3"></div>
-          <p>
-            “Cuộc sống sẽ như em hằng mong, hãy nuôi nhiều KHÁT VỌNG Thế giới
-            tạo [...]
-          </p>
+      <div class="grid grid-cols-3 px-[5%] lg:px-0 gap-x-[2%] text-center mb-8">
+        <div class="col-span-3 shadow-sm mt-[12%] lg:mt-0 lg:col-span-1">
+          <a href="">
+            <img src="src/assets/img/hinh10.png" alt="" class="w-full" />
+            <h2 class="font-bold mt-[2%]">
+              MV Hướng Dương – Wowy x Trung Nguyên Legend!
+            </h2>
+            <div class="bg-black w-10 h-px mx-auto my-3"></div>
+            <p class="hover:text-yellow-400">
+              “Cuộc sống sẽ như em hằng mong, hãy nuôi nhiều KHÁT VỌNG Thế giới
+              tạo [...]
+            </p>
+          </a>
         </div>
-        <div>
-          <img
-            src="src/assets/img/hinh11.jpg"
-            alt=""
-            class="max-w-full max-h-full h-60"
-          />
-          <h2 class="font-bold">Kỷ Niệm 1 Năm Ra Đời Trung Nguyên E-Coffee</h2>
-          <div class="bg-black w-10 h-px mx-auto my-3"></div>
-          <p>
-            Ngày 10/8/2020 đánh dấu cột mốc kỷ niệm 1 năm Trung Nguyên E-Coffee
-            chính thức [...]
-          </p>
+        <div class="col-span-3 shadow-sm mt-[12%] lg:mt-0 lg:col-span-1">
+          <a href="">
+            <img
+              src="src/assets/img/hinh11.jpg"
+              alt=""
+              class="w-full lg:h-[196.36px]"
+            />
+            <h2 class="font-bold mt-[2%]">
+              Kỷ Niệm 1 Năm Ra Đời Trung Nguyên E-Coffee
+            </h2>
+            <div class="bg-black w-10 h-px mx-auto my-3"></div>
+            <p class="hover:text-yellow-400">
+              Ngày 10/8/2020 đánh dấu cột mốc kỷ niệm 1 năm Trung Nguyên
+              E-Coffee chính thức [...]
+            </p>
+          </a>
         </div>
-        <div>
-          <img
-            src="src/assets/img/hinh12.jpg"
-            alt=""
-            class="max-w-full max-h-full h-60"
-          />
-          <h2 class="font-bold">
-            Trung Nguyên E-Coffee – Sự Lựa Chọn Hợp Tác Số 1 Của Hàng Ngàn Đối
-            Tác Trên Toàn Quốc
-          </h2>
-          <div class="bg-black w-10 h-px mx-auto my-3"></div>
-          <p>
-            Có đến Cần Thơ – “Thủ phủ miền Tây”, có gặp gỡ và chuyện trò [...]
-          </p>
+        <div class="col-span-3 shadow-sm mt-[12%] lg:mt-0 lg:col-span-1">
+          <a href="">
+            <img
+              src="src/assets/img/hinh12.jpg"
+              alt=""
+              class="w-full lg:h-[196.36px]"
+            />
+            <h2 class="font-bold mt-[2%]">
+              Trung Nguyên E-Coffee – Sự Lựa Chọn Hợp Tác Số 1 Của Hàng Ngàn Đối
+              Tác Trên Toàn Quốc
+            </h2>
+            <div class="bg-black w-10 h-px mx-auto my-3"></div>
+            <p class="hover:text-yellow-400">
+              Có đến Cần Thơ – “Thủ phủ miền Tây”, có gặp gỡ và chuyện trò [...]
+            </p>
+          </a>
         </div>
       </div>
     </div>
