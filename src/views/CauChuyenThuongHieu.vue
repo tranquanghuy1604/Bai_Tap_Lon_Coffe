2b0317f1dcb29d92c37bbb7f6726a51a42d44a0f
Ngb1thng <ngb1nhthng@gmail.com>
2022-06-19 22:19:11 +0700

    upload CauChuyenThuongHieu for Long

---
+++ src/views/CauChuyenThuongHieu.vue
@@ -0,0 +1,217 @@
+<template>
+    <div>
+        <div class="max-w-[1080px] mx-auto">
+        <!-- video -->
+        <div>
+            <video src="./img/cau-chuyen-thuong-hieu.mp4" preload="auto" autoplay muted loop ></video>
+        </div>
+        <!-- open paragraph -->
+        <div class="mx-10 my-10 font-sans">
+            <!-- paragraph1 -->
+            <p>Trung Nguyên E-Coffee là chuỗi cửa hàng bán lẻ thế giới cà phê của Tập đoàn Trung Nguyên Legend –
+                nơi hội tụ tinh hoa của 3 nền văn minh cà phê: Ottoman – Roman – Thiền được ra mắt từ tháng 8/2019. 
+                Ngay từ khi ra đời, Trung Nguyên E-Coffee nhanh chóng tạo nên một làn sóng bùng nổ nhượng quyền mạnh mẽ, 
+                trở thành sự lựa chọn số 1 của hơn 1.000 Đối Tác và hiện diện trên toàn quốc. 
+                Trung Nguyên E-Coffee được xem là một giải pháp tối ưu cho cộng đồng kinh doanh, 
+                khởi nghiệp với cà phê, thích hợp cho nhiều đối tượng, nhiều diện tích kinh doanh. <br><br></p>
+            <!-- paragraph2  -->
+            <p>Không chỉ sở hữu thiết kế linh hoạt cho mọi địa điểm, Trung Nguyên E-Coffee còn có chính sách hợp tác tối đa 
+                lợi ích nhất cho tất cả các Đối Tác từ chính sách gói hợp tác đến các chương trình hỗ trợ đào tạo, tư vấn, vận hành… 
+                Hơn nữa, với hệ sản phẩm đặc biệt gồm hơn 100 sản phẩm cà phê năng lượng tuyệt ngon – sách quý đổi đời – bánh mì 
+                thực dưỡng, Trung Nguyên E-Coffee tạo nên một thế giới cà phê thu nhỏ, đáp ứng cho những người yêu cà phê 
+                có thể trải nghiệm đầy đủ các gói sản phẩm thưởng lãm tại nhà, văn phòng, hàng quán…</p>
+        </div>
+        <div class="text-center text-xl font-bold">
+            <p>
+                <span>NHỮNG DẤU ẤN ĐẶC BIỆT TRÊN HÀNH TRÌNH PHÁT TRIỂN</span><br>
+                <span>CỦA TRUNG NGUYÊN E-COFFEE</span>
+            </p>
+        </div>
+        <div class="bg-black w-11 h-0.5 mx-auto my-7"></div>
+        <div class="mx-40 font-sans mb-8">
+            <p>
+                Kể từ ngày ra mắt mô hình Nhượng Quyền 0 Đồng – 10/08/2019, 
+                Trung Nguyên E-Coffee chính thức khai trương và đi vào hoạt động gần <strong>600 cửa hàng</strong>  
+                và hơn <strong>1000 hợp đồng</strong>  được ký mới trên khắp các tỉnh thành Việt Nam.
+            </p>
+        </div>
+        <!-- timeline right -->
+        <div class="grid grid-cols-5 gap-1 mx-10">
+            <!-- logo -->
+            <div class="col-start-3 col-span-1  bg-white mx-6 "> 
+                <div class="flex justify-center" >
+                    <div class="box-border aspect-square block border-solid border-2 border-yellow-400 rounded-full hover:bg-yellow-400 p-3">
+                        <img src="./img/2020.png" alt="">
+                    </div>    
+                </div>
+                <div class="w-2/4 border-dashed border-gray-400 border-r-2 h-full"></div>
+            </div>
+            <!-- paragraph -->
+            <div class="col-span-2">
+                <div class="border-gray-400 border-solid border-b-2 border-r-2 border-l-2">
+                    <div class="bg-black h-12 font-bold text-white text-lg my-auto flex items-center pl-2"> 2020</div>
+                    <div class="bg-white mx-3 " >
+                        <h2 class="font-bold text-center leading-8 text-lg mt-2 mb-3">RA MẮT PHIÊN BẢN <br> 3 NỀN VĂN MINH CÀ PHÊ</h2>
+                        <p>Ngày 25/6/2020, chương trình “Kết nối – Thành công cùng Trung Nguyên E-Coffee” 
+                            được tổ chức tại Cần Thơ, Trung Nguyên E-Coffee chính thức ra mắt phiên bản mới 2020: 
+                            <strong>Hội tụ 3 nền văn minh cà phê thế giới Ottoman – Roman – Thiền.</strong> </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- timeline left -->
+        <div class="grid grid-cols-5 gap-1 mx-10">
+            <!-- paragraph -->
+            <div class="col-span-2">
+                <div class="border-gray-400 border-solid border-b-2 border-r-2 border-l-2">
+                    <div class="bg-black h-12 font-bold text-white text-lg my-auto flex items-center pl-2"> 2019</div>
+                    <div class="bg-white mx-3">
+                        <h2 class="font-bold text-center leading-8 text-lg mt-2 mb-3">RA MẮT MÔ HÌNH <br> NHƯỢNG QUYỀN 0 ĐỒNG</h2>
+                        <p>Ngày 10/08/2019, Trung Nguyên E-Coffee ra mắt mô hình nhượng quyền 0 đồng 
+                           <strong>“Kết Nối Thành Công Cùng Trung Nguyên E-Coffee”</strong>  tại 
+                           <strong>Trung Tâm Hội Nghị 272 Võ Thị Sáu, Quận 3.</strong></p>
+                    </div>
+                </div>
+            </div>
+            <!-- logo -->
+            <div class="col-span-1 bg-white mx-6 "> 
+                <div class="flex justify-center" >
+                    <div class="box-border aspect-square block border-solid border-2 border-yellow-400 rounded-full hover:bg-yellow-400 p-3">
+                        <img src="./img/2019.png" alt="" class="">
+                    </div>    
+                </div>
+                <div class="w-2/4 border-dashed border-gray-400 border-r-2 h-full"></div>
+            </div>
+        </div>
+        <!-- timeline right -->
+        <div class="grid grid-cols-5 gap-1 mx-10">
+            <!-- logo -->
+            <div class="col-start-3 col-span-1 bg-white mx-6 "> 
+                <div class="flex justify-center" >
+                    <div class="box-border aspect-square block border-solid border-2 border-yellow-400 rounded-full hover:bg-yellow-400 p-3">
+                        <img src="./img/2018.png" alt="">
+                    </div>    
+                </div>
+                <div class="w-2/4 border-dashed border-gray-400 border-r-2 h-full"></div>
+            </div>
+            <!-- paragraph -->
+            <div class=" col-span-2 ">
+                <div class="border-gray-400 border-solid border-b-2 border-r-2 border-l-2">
+                    <div class="bg-black h-12 font-bold text-white text-lg my-auto flex items-center pl-2"> 2018</div>
+                    <div class="bg-white mx-3">
+                        <h2 class="font-bold text-center leading-8 text-lg mt-2 mb-3">KHÁNH THÀNH <br> BẢO TÀNG THẾ GIỚI CÀ PHÊ</h2>
+                        <p>Ngày 23/11/2018, Tập đoàn Trung Nguyên Legend tự hào đánh dấu cột mốc mới trên hành trình góp 
+                            phần định vị lại giá trị của ngành cà phê Việt Nam khi chính thức khánh thành 
+                            <strong>Bảo tàng Thế giới Cà phê</strong>  trong khuôn viên Dự án Thành phố Cà phê 
+                            rộng hơn 45ha tại Tp Buôn Ma Thuột.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- timeline left -->
+        <div class="grid grid-cols-5 gap-1 mx-10">
+            <!-- paragraph -->
+            <div class=" col-span-2">
+                <div class="border-gray-400 border-solid border-b-2 border-r-2 border-l-2">
+                    <div class="bg-black h-12 font-bold text-white text-lg my-auto flex items-center pl-2"> 2017</div>
+                    <div class="bg-white mx-3">
+                        <h2 class="font-bold text-center leading-8 text-lg mt-2 mb-3">RA MẮT CỬA HÀNG ĐẦU TIÊN</h2>
+                        <p>Ngày 20/12/2017, ra mắt <strong>CỬA HÀNG ĐẦU TIÊN</strong>  
+                            tại địa chỉ <strong>324 Nguyễn Đình Chiểu, Phường 4, Quận 3.</strong> </p>
+                    </div>
+                </div>
+            </div>
+            <!-- logo -->
+            <div class="col-span-1 bg-white mx-6 "> 
+                <div class="flex justify-center" >
+                    <div class="box-border aspect-square block border-solid border-2 border-yellow-400 rounded-full hover:bg-yellow-400 p-3">
+                        <img src="./img/2017.png" alt="">
+                    </div>    
+                </div>
+                <div class="w-2/4 border-dashed border-gray-400 border-r-2 h-full"></div>
+            </div>
+        </div>
+        <!-- Title -->
+        <div class="px-11 justify-center box-content font-bold mt-32">
+            <h2 class="text-left text-lg">HÀNH TRÌNH TỪ TRÁI TIM</h2>
+            <h3 class="text-right">Xem tất cả  ></h3>
+            <div class="bg-black w-auto h-px mb-10"></div>
+        </div>
+
+        <!-- items -->
+        <div class="grid lg:grid-cols-3 md:grid-cols-1 text-center justify-center gap-8 mx-20">
+            <div class="col-span-3 lg:col-span-1">
+                <img src="./img/hinh1.jpg" alt="" class="w-full h-60"/>
+                <h2 class="font-bold">Hành Trình Từ Trái Tim Tri Thức Là Nguồn Tài Sản Quý Giá Nhất</h2>
+                <div class="bg-black w-10 h-px mx-auto my-3"></div>
+                <p class="hover:text-yellow-400">Ngày 16 – 17.8.2019, Hành trình Từ Trái Tim đã đến với TP.Đà Nẵng cùng [...]</p>
+            </div>
+            <div class="col-span-3 lg:col-span-1">
+                <img src="./img/hinh2.jpg" alt="" class="w-full h-60">
+                <h2 class="font-bold">Hành Trình Đến Với Chiến Sĩ Hải Quân Và Bộ Đội Biên Phòng Tỉnh Quảng Ninh</h2>
+                <div class="bg-black w-10 h-px mx-auto my-3"></div>
+                <p class="hover:text-yellow-400">Chiều 16.6, ngay sau lễ xuất hành tại Trường Đại học Hàng Hải, “Hành trình [...]</p>
+            </div>
+            <div class="col-span-3 lg:col-span-1">
+                <img src="./img/hinh3.jpg" alt="" class="w-full h-60">
+                <h2 class="font-bold">Trung Nguyên E-Coffee Một Mô Hình Khởi Nghiệp Đồng Hành Với Hành Trình Từ Trái Tim</h2>
+                <div class="bg-black w-10 h-px mx-auto my-3"></div>
+                <p class="hover:text-yellow-400">Tham gia Hành Trình Từ Trái Tim thông qua Tủ Sách Nền Tảng Đổi Đời [...]</p>
+            </div>
+        </div>
+
+        <!-- Title -->
+        <div class="px-11 justify-center box-content font-bold mt-32">
+            <h1 class="text-left text-lg">KIẾN THỨC CÀ PHÊ</h1>
+            <h3 class="text-right">Xem tất cả  ></h3>
+            <div class="bg-black w-auto h-px mb-10"></div>
+        </div>
+
+        <!-- items -->
+        <div class="grid lg:grid-cols-3 md:grid-cols-1 mx-10 gap-8 text-center">
+            <div class="col-span-3 lg:col-span-1">
+                <img src="./img/hinh4.jpg" alt="" class="w-full h-60" >
+                <h2 class="font-bold">Kỳ 49: Đại Văn Hào Johann Wolfgang Von Goethe – Cà Phê Là Thức Uống Tuyệt Vời Nhất!</h2>
+                <div class="bg-black w-10 h-px mx-auto my-3"></div>
+                <p class="hover:text-yellow-400">Người Nhật đã định hình Trà đạo thành đạo lý của dân tộc, [...]</p>
+            </div>
+            <div class="col-span-3 lg:col-span-1">
+                <img src="./img/hinh5.jpg" alt="" class="w-full h-60">
+                <h2 class="font-bold">Kỳ 48: Ludwig Van Beethoven – Cà Phê Và Những Bản Giao Hưởng Khát Vọng Hạnh Phúc</h2>
+                <div class="bg-black w-10 h-px mx-auto my-3"></div>
+                <p class="hover:text-yellow-400">Người Nhật đã định hình Trà đạo thành đạo lý của dân tộc, một [...]</p>
+            </div>
+            <div class="col-span-3 lg:col-span-1">
+                <img src="./img/hinh6.jpg" alt="" class="w-full h-60">
+                <h2 class="font-bold">Kỳ 47: Cà Phê Trong Tiến Trình Thăng Hoa Âm Nhạc Của Johann Sebastian Bach</h2>
+                <div class="bg-black w-10 h-px mx-auto my-3"></div>
+                <p class="hover:text-yellow-400">Người Nhật đã định hình Trà đạo thành đạo lý của dân tộc, một [...]</p>
+            </div>
+        </div>
+        <div class="border-solid border-2 border-gray-400 mt-32 mb-10 mx-auto text-center font-bold text-xl">
+            SÁCH QUÝ NỀN TẢNG ĐỔI ĐỜI</div>
+        <div class="grid lg:grid-cols-5 md:grid-cols-2 gap-4 text-center">
+            <div class="lg:grid-cols-1 md:col-span-1">
+                <img src="./img/sach1.png" alt="">
+                <h2>Không Bao Giờ Là Thất Bại,<br> Tất Cả Là Thử Thách</h2>
+            </div>
+            <div class="lg:grid-cols-1 md:col-span-1">
+                <img src="./img/sach2.png" alt="">
+                <h2>Khuyến Học</h2>
+            </div>
+            <div class="lg:grid-cols-1 md:col-span-1">
+                <img src="./img/sach3.png" alt="">
+                <h2>Nghĩ Giàu Làm Giàu</h2>
+            </div>
+            <div class="lg:grid-cols-1 md:col-span-1">
+                <img src="./img/sach4.png" alt="">
+                <h2>Đắc Nhân Tâm</h2>
+            </div>
+            <div class="lg:grid-cols-1 md:col-span-1">
+                <img src="./img/sach5.png" alt="">
+                <h2>Quốc Gia Khởi Nghiệp</h2>
+            </div>
+        </div>
+    </div>
+    </div>
+</template>
